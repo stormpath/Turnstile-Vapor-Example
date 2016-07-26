d71aef95c7ebfbe7a69d4e98ad7160ee7c6704f4
Edward Jiang <edward@stormpath.com>
2016-07-26 13:39:23 -0700

    initial vapor project setup

---
+++ Package.swift
@@ -0,0 +1,17 @@
+import PackageDescription
+
+let package = Package(
+    name: "VaporApp",
+    dependencies: [
+        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 14),
+        .Package(url: "https://github.com/qutheory/vapor-mustache.git", majorVersion: 0, minor: 10)
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+        "Tests",
+    ]
+)
